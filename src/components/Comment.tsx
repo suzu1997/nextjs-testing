2091d226b0fb8fe4a16af5ff1c428ffd381f45a0
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-12-29 15:15:31 +0900

    Commentコンポーネントを作成し、コメントページでfetchしたデータを表示

---
+++ src/components/Comment.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+import { Comment } from '../types/types';
+
+export const CommentComponent: FC<Comment> = (props) => {
+  const { id, name, body } = props;
+  return (
+    <li className='mx-10'>
+      <p className=''>
+        {id} : {body}
+      </p>
+      <p className='text-center mt-3 mb-10'>by {name}</p>
+    </li>
+  );
+};
--- src/pages/comment-page.tsx
+++ src/pages/comment-page.tsx
@@ -1,10 +1,31 @@
 import { NextPage } from 'next';
 import { Layout } from '../components/Layout';
+import useSWR from 'swr';
+import axios from 'axios';
+import { CommentComponent } from '../components/Comment';
+import { Comment } from '../types/types';
+
+const axiosFetcher = async () => {
+  const result = await axios.get<Comment[]>(
+    'https://jsonplaceholder.typicode.com/comments/?_limit=10',
+  );
+  return result.data;
+};
 
 const CommentPage: NextPage = () => {
+  const { data: comments, error } = useSWR('commentsFetch', axiosFetcher);
+
+  if (error) return <span>Error!</span>;
+
   return (
     <Layout title='Comment'>
-      <p className='text-4xl'>Comment Page</p>
+      <p className='text-4xl m-10'>Comment Page</p>
+      <ul>
+        {comments &&
+          comments.map((comment) => (
+            <CommentComponent key={comment.id} {...comment} />
+          ))}
+      </ul>
     </Layout>
   );
 };
