d71728d69ed605f27b9f7c7720119b48f2bda95c
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-12-31 11:22:11 +0900

    task-pageにおける、getStaticPropsのテストファイルを作成

---
+++ __tests__/TaskPageStatic.test.tsx
@@ -0,0 +1,60 @@
+/**
+ * @jest-environment jsdom
+ */
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen, cleanup } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { getPage } from 'next-page-tester';
+import { initTestHelpers } from 'next-page-tester';
+
+initTestHelpers();
+
+const handlers = [
+  rest.get(
+    'https://jsonplaceholder.typicode.com/todos/?_limit=10',
+    (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json([
+          {
+            userId: 1,
+            id: 1,
+            title: 'Static task 1',
+            completed: false,
+          },
+          {
+            userId: 2,
+            id: 2,
+            title: 'Static task 2',
+            completed: false,
+          },
+        ]),
+      );
+    },
+  ),
+];
+
+const server = setupServer(...handlers);
+beforeAll(() => {
+  server.listen();
+});
+afterEach(() => {
+  server.resetHandlers();
+  cleanup();
+});
+afterAll(() => {
+  server.close();
+});
+
+describe('Todo page / getStaticProps', () => {
+  it('Shold render the list of tasks pre-fetched by getStaticProps', async () => {
+    const { page } = await getPage({
+      route: '/task-page',
+    });
+    render(page);
+    expect(await screen.findByText('Todos Page')).toBeInTheDocument();
+    expect(screen.getByText('1: Static task 1')).toBeInTheDocument();
+    expect(screen.getByText('2: Static task 2')).toBeInTheDocument();
+  });
+});
